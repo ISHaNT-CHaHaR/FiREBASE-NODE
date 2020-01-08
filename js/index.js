cef93569ba0d54e43e574062b5bae22eceb210d2
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-01-08 19:05:32 +0530

    sign in completed!
    Sign in can be doen in signin.js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,29 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
-   $("#btn-login.click").click(function(){
+   $("#btn-login").click(function(){
+         var email= $("#email").val();
+         var password= $("#password").val();
          
-   });
+         firebase.auth.Auth.Persistence.LOCAL;
+
+         if(email!="" && password!= "")
+         {
+                  var result = firebase.auth().signInWithEmailAndPassword(email,password);
+                  result.catch(function(error){
+                      var errorCode= error.code;
+                      var errorMessage= error.message;
+                      
+                      console.log(errorCode);
+                      console.log(errorMessage);
+
+                      window.alert("Message : " + errorMessage);
+
+
+                  } );
+         }
+         else{
+             window.alert("Please fill out all fields!");
+         }
+   
+        });
