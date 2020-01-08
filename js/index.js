2fa6cc382da6092966de77fc06cc29545a37b772
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-01-08 17:07:55 +0530

    firebase config .
    firebase configuration is done ans index.js added.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCsCRoPyTpCznfEpv6UBOJVKnv_b7Qkq2U",
+    authDomain: "fir-webapp-e093f.firebaseapp.com",
+    databaseURL: "https://fir-webapp-e093f.firebaseio.com",
+    projectId: "fir-webapp-e093f",
+    storageBucket: "fir-webapp-e093f.appspot.com",
+    messagingSenderId: "215898760335",
+    appId: "1:215898760335:web:e44df0b217cb90ce3cfcfa",
+    measurementId: "G-076L1V04EH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+   $("#btn-login.click").click(function(){
+         
+   });
